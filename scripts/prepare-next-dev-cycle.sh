3bd493e945b942c8b1a7bdfa5d1f86bf25d661b4
Robert Stoll <robert.stoll@tegonal.com>
2022-06-22 14:08:41 +0200

    add release workflow part 2 (not all changes saved)

--- scripts/prepare-next-dev-cycle.sh
+++ scripts/prepare-next-dev-cycle.sh
@@ -12,5 +12,5 @@ echo "prepare next dev cycle for version $version"
 current_dir="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> /dev/null; )";
 
 "$current_dir/../src/releasing/sneak-peek-banner.sh" -c show
-"$current_dir/../src/releasing/toggle-sections" -v main
+"$current_dir/../src/releasing/toggle-sections.sh" -c main
 "$current_dir/../src/releasing/update-version-scripts.sh" -v "$version-SNAPSHOT"
