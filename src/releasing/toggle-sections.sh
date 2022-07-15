86a6b3705a8da840d68649a9d49d8149024be059
Robert Stoll <robert.stoll@tegonal.com>
2022-07-15 15:22:44 +0200

    prepare next dev cycle for v0.7.0

--- src/releasing/toggle-sections.sh
+++ src/releasing/toggle-sections.sh
@@ -5,7 +5,7 @@
 #  / __/ -_) _ `/ _ \/ _ \/ _ `/ /        It is licensed under Apache 2.0
 #  \__/\__/\_, /\___/_//_/\_,_/_/         Please report bugs and contribute back your improvements
 #         /___/
-#                                         Version: v0.6.1
+#                                         Version: v0.7.0-SNAPSHOT
 #
 #######  Description  #############
 #
@@ -26,7 +26,7 @@
 #
 ###################################
 set -eu
-declare -x TEGONAL_SCRIPTS_VERSION='v0.6.1'
+declare -x TEGONAL_SCRIPTS_VERSION='v0.7.0-SNAPSHOT'
 
 if ! [[ -v dir_of_tegonal_scripts ]]; then
 	dir_of_tegonal_scripts="$(realpath "$(cd -- "$(dirname -- "${BASH_SOURCE[0]:-$0}")" &>/dev/null && pwd 2>/dev/null)/..")"
