a3030e94c929ab1c25a373c6a35605cc6761c146
Robert Stoll <robert.stoll@tegonal.com>
2022-07-15 16:08:27 +0200

    prepare next dev cycle for v0.8.0

--- src/releasing/update-version-scripts.sh
+++ src/releasing/update-version-scripts.sh
@@ -5,7 +5,7 @@
 #  / __/ -_) _ `/ _ \/ _ \/ _ `/ /        It is licensed under Apache 2.0
 #  \__/\__/\_, /\___/_//_/\_,_/_/         Please report bugs and contribute back your improvements
 #         /___/
-#                                         Version: v0.7.0
+#                                         Version: v0.8.0-SNAPSHOT
 #
 #######  Description  #############
 #
@@ -22,7 +22,7 @@
 #
 ###################################
 set -eu
-declare -x TEGONAL_SCRIPTS_VERSION='v0.7.0'
+declare -x TEGONAL_SCRIPTS_VERSION='v0.8.0-SNAPSHOT'
 
 if ! [[ -v dir_of_tegonal_scripts ]]; then
 	dir_of_tegonal_scripts="$(realpath "$(cd -- "$(dirname -- "${BASH_SOURCE[0]:-$0}")" &>/dev/null && pwd 2>/dev/null)/..")"
