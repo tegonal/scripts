44e1f548b6caae4ba74d22b7ec0943a7b490b957
Robert Stoll <robert.stoll@tegonal.com>
2022-07-20 15:12:27 +0200

    add withCustomOutputInput

--- README.md
+++ README.md
@@ -109,6 +109,7 @@ The scripts are ordered by topic:
     - [`source` once](#source-once)
     - [git Utils](#git-utils)
     - [GPG Utils](#gpg-utils)
+    - [IO functions](#io-functions)
     - [Recursive `declare -p`](#recursive-declare--p)
     - [Replace Snippets](#replace-snippets)
     - [Update Documentation](#update-bash-documentation)
@@ -818,6 +819,35 @@ trustGpgKey ~/.gpg info.com
 ```
 
 </utility-gpg-utils>
+
+## IO functions
+
+<utility-io>
+
+<!-- auto-generated, do not modify here but in src/utility/io.sh -->
+```bash
+#!/usr/bin/env bash
+set -euo pipefail
+# Assumes tegonal's scripts were fetched with gget - adjust location accordingly
+dir_of_tegonal_scripts="$(realpath "$(cd -- "$(dirname -- "${BASH_SOURCE[0]:-$0}")" &>/dev/null && pwd 2>/dev/null)/../lib/tegonal-scripts/src")"
+source "$dir_of_tegonal_scripts/setup.sh" "$dir_of_tegonal_scripts"
+
+sourceOnce "$dir_of_tegonal_scripts/utility/io.sh"
+
+function readFile() {
+	cat "$1" >&3
+	echo "reading from 4 which was written to 3"
+	while read -u 4 -r line; do
+		echo "$line"
+	done
+}
+
+# creates file descriptors 3 (output) and 4 (input) based on temporary files
+# executes readFile and closes the file descriptors again
+withCustomOutputInput 3 4 readFile
+```
+
+</utility-io>
 
 ## Recursive `declare -p`
 
---
+++ src/utility/io.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+#
+#    __                          __
+#   / /____ ___ ____  ___  ___ _/ /       This script is provided to you by https://github.com/tegonal/scripts
+#  / __/ -_) _ `/ _ \/ _ \/ _ `/ /        It is licensed under Apache 2.0
+#  \__/\__/\_, /\___/_//_/\_,_/_/         Please report bugs and contribute back your improvements
+#         /___/
+#                                         Version: v0.11.0-SNAPSHOT
+#
+#######  Description  #############
+#
+#  utility function dealling with Input/Ouput
+#
+#######  Usage  ###################
+#
+#    #!/usr/bin/env bash
+#    set -euo pipefail
+#    # Assumes tegonal's scripts were fetched with gget - adjust location accordingly
+#    dir_of_tegonal_scripts="$(realpath "$(cd -- "$(dirname -- "${BASH_SOURCE[0]:-$0}")" &>/dev/null && pwd 2>/dev/null)/../lib/tegonal-scripts/src")"
+#    source "$dir_of_tegonal_scripts/setup.sh" "$dir_of_tegonal_scripts"
+#
+#    sourceOnce "$dir_of_tegonal_scripts/utility/io.sh"
+#
+#    function readFile() {
+#    	cat "$1" >&3
+#    	echo "reading from 4 which was written to 3"
+#    	while read -u 4 -r line; do
+#    		echo "$line"
+#    	done
+#    }
+#
+#    # creates file descriptors 3 (output) and 4 (input) based on temporary files
+#    # executes readFile and closes the file descriptors again
+#    withCustomOutputInput 3 4 readFile
+#
+###################################
+set -euo pipefail
+
+if ! [[ -v dir_of_tegonal_scripts ]]; then
+	dir_of_tegonal_scripts="$(realpath "$(cd -- "$(dirname -- "${BASH_SOURCE[0]:-$0}")" &>/dev/null && pwd 2>/dev/null)/..")"
+	source "$dir_of_tegonal_scripts/setup.sh" "$dir_of_tegonal_scripts"
+fi
+
+function withCustomOutputInput() {
+	local outputNr=$1
+	local inputNr=$2
+	local fun=$3
+	shift 3
+
+	local tmpFile
+	tmpFile=$(mktemp /tmp/tegonal-scripts-io.XXXXXXXXX)
+	eval "exec${outputNr}>\"$tmpFile\""
+	eval "exec${inputNr}<\"$tmpFile\""
+	rm "$tmpFile"
+
+	$fun
+
+	eval "exec ${outputNr}>&-"
+	eval "exec ${inputNr}<&-"
+}
