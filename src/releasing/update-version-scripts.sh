781c23b5df7aecb470556bd15b39b0acf0ca02ce
Robert Stoll <robert.stoll@tegonal.com>
2022-09-13 22:50:48 +0200

    improve doc about --directory, mention that it can be a file

--- src/releasing/update-version-scripts.sh
+++ src/releasing/update-version-scripts.sh
@@ -45,7 +45,7 @@ function updateVersionScripts() {
 	# shellcheck disable=SC2034
 	local -ra params=(
 		version '-v' 'the version which shall be used'
-		directory '-d|--directory' '(optional) the working directory -- default: ./src'
+		directory '-d|--directory' '(optional) the working directory in which *.sh are searched (also in subdirectories) / you can also specify a file -- default: ./src'
 		additionalPattern '-p|--pattern' '(optional) pattern which is used in a perl command (separator /) to search & replace additional occurrences. It should define two match groups and the replace operation looks as follows: '"\\\${1}\$version\\\${2}"
 	)
 	local -r examples=$(
@@ -68,7 +68,13 @@ function updateVersionScripts() {
 	if ! [[ -v additionalPattern ]]; then additionalPattern=""; fi
 	exitIfNotAllArgumentsSet params "$examples" "$TEGONAL_SCRIPTS_VERSION"
 
-	echo "set version $version in bash headers in directory $directory"
+	local where
+	if [[ -f $directory ]]; then
+		where="in file $directory"
+	else
+		where="directory $directory (and subdirectories)"
+	fi
+	echo "set version $version in bash headers in $where"
 	if [[ -n $additionalPattern ]]; then
 		echo "also going to search for $additionalPattern and replace with \${1}$version\${2}"
 	fi
