809139ea2e242eefa345f2983e8c312bdd9345dc
Robert Stoll <robert.stoll@tegonal.com>
2022-09-19 17:40:18 +0200

    rename update-docu to cleanup-....sh to comply with cleanup.yml
    moreover:
    - fix update-bash-docu.doc.sh is using an old function name

--- src/utility/update-bash-docu.doc.sh
+++ src/utility/update-bash-docu.doc.sh
@@ -9,10 +9,8 @@ source "$dir_of_tegonal_scripts/utility/update-bash-docu.sh"
 
 find . -name "*.sh" \
 	-not -name "*.doc.sh" \
-	-not -path "**.history/*" \
-	-not -name "update-docu.sh" \
 	-print0 |
 	while read -r -d $'\0' script; do
 		declare script="${script:2}"
-		replaceSnippetForScript "$dir_of_tegonal_scripts/$script" "${script////-}" . README.md
+		updateBashDocumentation "$dir_of_tegonal_scripts/$script" "${script////-}" . README.md
 	done
