f2154aa7544d30f94d27f7c5da32fa8f3aabb5df
Robert Stoll <robert.stoll@tegonal.com>
2024-03-06 16:17:31 +0100

    add copyright to the license header

--- scripts/additional-prepare-files-next-dev-cycle-steps.sh
+++ scripts/additional-prepare-files-next-dev-cycle-steps.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 #
 #    __                          __
-#   / /____ ___ ____  ___  ___ _/ /       This script is provided to you by https://github.com/tegonal/github-commons
-#  / __/ -_) _ `/ _ \/ _ \/ _ `/ /        It is licensed under Creative Commons Zero v1.0 Universal
-#  \__/\__/\_, /\___/_//_/\_,_/_/         Please report bugs and contribute back your improvements
-#         /___/
-#                                         Version: v1.3.0-SNAPSHOT
+#   / /____ ___ ____  ___  ___ _/ /       This script is provided to you by https://github.com/tegonal/scripts
+#  / __/ -_) _ `/ _ \/ _ \/ _ `/ /        Copyright 2022 Tegonal Genossenschaft <info@tegonal.com>
+#  \__/\__/\_, /\___/_//_/\_,_/_/         It is licensed under Apache License 2.0
+#         /___/                           Please report bugs and contribute back your improvements
 #
+#                                         Version: v1.3.0-SNAPSHOT
 ###################################
 set -euo pipefail
 shopt -s inherit_errexit
