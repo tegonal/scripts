c86bdb0b6e1d55c9447ccc2501c1c63415ac4b55
Robert Stoll <robert.stoll@tegonal.com>
2024-03-07 16:44:54 +0100

    prepare next dev cycle for v1.4.0

--- src/qa/run-shellcheck.sh
+++ src/qa/run-shellcheck.sh
@@ -5,7 +5,7 @@
 #  / __/ -_) _ `/ _ \/ _ \/ _ `/ /        It is licensed under Apache License 2.0
 #  \__/\__/\_, /\___/_//_/\_,_/_/         Please report bugs and contribute back your improvements
 #         /___/
-#                                         Version: v1.3.1
+#                                         Version: v1.4.0-SNAPSHOT
 #
 #######  Description  #############
 #
--- src/releasing/update-version-issue-templates.sh
+++ src/releasing/update-version-issue-templates.sh
@@ -5,7 +5,7 @@
 #  / __/ -_) _ `/ _ \/ _ \/ _ `/ /        It is licensed under Apache License 2.0
 #  \__/\__/\_, /\___/_//_/\_,_/_/         Please report bugs and contribute back your improvements
 #         /___/
-#                                         Version: v1.3.1
+#                                         Version: v1.4.0-SNAPSHOT
 #
 #######  Description  #############
 #
@@ -32,7 +32,7 @@
 set -euo pipefail
 shopt -s inherit_errexit
 unset CDPATH
-export TEGONAL_SCRIPTS_VERSION='v1.3.1'
+export TEGONAL_SCRIPTS_VERSION='v1.4.0-SNAPSHOT'
 
 if ! [[ -v dir_of_tegonal_scripts ]]; then
 	dir_of_tegonal_scripts="$(cd -- "$(dirname -- "${BASH_SOURCE[0]:-$0}")" >/dev/null && pwd 2>/dev/null)/.."
