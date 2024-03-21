53bdcb8376f4231ae5b1027573332f04fd69c86c
Robert Stoll <robert.stoll@tegonal.com>
2024-03-21 18:19:12 +0100

    fix scope

--- scripts/prepare-next-dev-cycle.sh
+++ scripts/prepare-next-dev-cycle.sh
@@ -34,7 +34,7 @@ function prepareNextDevCycle() {
 	source "$dir_of_tegonal_scripts/releasing/common-constants.source.sh" || die "could not source common-constants.source.sh"
 
 	# shellcheck disable=SC2034   # they seem unused but are necessary in order that parseArguments doesn't create global readonly vars
-	local version projectsRootDir additionalPattern
+	local version projectsRootDir additionalPattern beforePrFn
 	# shellcheck disable=SC2034   # is passed by name to parseArguments
 	local -ra params=(
 		version "$versionParamPattern" 'the version for which we prepare the dev cycle'
