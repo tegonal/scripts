98f191eda282a6e6d432a6e7495cf2ed2a9fb734
Robert Stoll <robert.stoll@tegonal.com>
2024-04-04 22:18:43 +0200

    allow to use version without leading v in download banner
    i.e. if one uses version without leading v then we replace it with the
    new version also without leading v

--- src/releasing/update-version-README.sh
+++ src/releasing/update-version-README.sh
@@ -72,8 +72,11 @@ function updateVersionReadme() {
 
 	echo "set version $version for Download badges and sneak peek banner in $file"
 
+	local -r versionWithoutLeadingV="${version:1}"
+
 	perl -0777 -i \
-		-pe "s@(\[!\[Download\]\(https://img.shields.io/badge/Download-).*(-%23[0-9a-f]+\)\]\([^\)]+(?:=|/))[^\)]+\)@\${1}$version\${2}$version\)@g;" \
+		-pe "s@(\[!\[Download\]\(https://img.shields.io/badge/Download-).*(-%23[0-9a-f]+\)\]\([^\)]+(?:=|/))v[^\)]+\)@\${1}$version\${2}$version\)@g;" \
+		-pe "s@(\[!\[Download\]\(https://img.shields.io/badge/Download-).*(-%23[0-9a-f]+\)\]\([^\)]+(?:=|/))[0-9][^\)]+\)@\${1}$version\${2}$versionWithoutLeadingV\)@g;" \
 		-pe "s@(\[README of )[^\]]+(\].*/tree/)[^/]+/@\${1}$version\${2}$version/@;" \
 		"$file" || returnDying "was not able to update the version in download badges and in the sneak peek banner" || return $?
 
