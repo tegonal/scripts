45fcd9ef081f45951f21163b5c1ab776a2857172
Robert Stoll <robert.stoll@tegonal.com>
2024-10-24 20:41:28 +0200

    importGpgKey require confirmationQuestion instead of --confirm=true

--- src/utility/gpg-utils.doc.sh
+++ src/utility/gpg-utils.doc.sh
@@ -7,14 +7,16 @@ source "$dir_of_tegonal_scripts/setup.sh" "$dir_of_tegonal_scripts"
 
 sourceOnce "$dir_of_tegonal_scripts/utility/gpg-utils.sh"
 
-# import public-key.asc into gpg store located at ~/.gpg but ask for confirmation first
-importGpgKey ~/.gpg ./public-key.asc --confirmation=true
-
 # import public-key.asc into gpg store located at ~/.gpg and trust automatically
-importGpgKey ~/.gpg ./public-key.asc --confirmation=false
+importGpgKey ~/.gpg ./public-key.asc
 
-# import public-key.asc into gpg store located at .gt/.gpg and trust automatically
-importGpgKey .gt/.gpg ./public-key.asc --confirmation=false
+# import public-key.asc into gpg store located at ~/.gpg but ask given question first which needs to be answered with yes
+importGpgKey ~/.gpg ./public-key.asc "do you want to import the shown key(s)?"
+
+# import public-key.asc into gpg store located at .gt/remotes/tegonal-scripts/public-keys/gpg
+# and trust automatically
+importGpgKey .gt/remotes/tegonal-scripts/public-keys/gpg ./public-key.asc
 
 # trust key which is identified via info@tegonal.com in gpg store located at ~/.gpg
 trustGpgKey ~/.gpg info@tegonal.com
+
