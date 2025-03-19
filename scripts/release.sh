6076de21b64017da47397a49bc1e963fdd99d8df
Robert Stoll <robert.stoll@tegonal.com>
2025-03-19 08:55:30 +0100

    prepare next dev cycle for v4.5.0

--- scripts/release.sh
+++ scripts/release.sh
@@ -6,12 +6,12 @@
 #  \__/\__/\_, /\___/_//_/\_,_/_/         It is licensed under Apache License 2.0
 #         /___/                           Please report bugs and contribute back your improvements
 #
-#                                         Version: v4.4.3
+#                                         Version: v4.5.0-SNAPSHOT
 ###################################
 set -euo pipefail
 shopt -s inherit_errexit
 unset CDPATH
-export TEGONAL_SCRIPTS_VERSION='v4.4.3'
+export TEGONAL_SCRIPTS_VERSION='v4.5.0-SNAPSHOT'
 
 if ! [[ -v scriptsDir ]]; then
 	scriptsDir="$(cd -- "$(dirname -- "${BASH_SOURCE[0]:-$0}")" >/dev/null && pwd 2>/dev/null)"
