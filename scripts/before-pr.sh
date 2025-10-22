bd53113323d828f026a1fc8a1de9b07cb409963c
Robert Stoll <robert.stoll@tegonal.com>
2025-10-22 17:19:56 +0200

    cleanup first and then shellcheck as formatting could break something

--- scripts/before-pr.sh
+++ scripts/before-pr.sh
@@ -31,10 +31,10 @@ sourceOnce "$scriptsDir/run-shellcheck.sh"
 function beforePr() {
 	# using && because this function is used on the left side of an || in releaseFiles
 	# this way we still have fail fast behaviour and don't mask/hide a non-zero exit code
-	runShellspecIfInstalled --jobs 2 &&
+	cleanupOnPushToMain &&
+		runShellspecIfInstalled --jobs 2 &&
 		checkInBugTemplate &&
-		customRunShellcheck &&
-		cleanupOnPushToMain
+		customRunShellcheck
 }
 
 ${__SOURCED__:+return}
